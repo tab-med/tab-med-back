bedfe498d3d2c1896b3c315147968a1a07837568
arthur v <hayashiifunky@gmail.com>
2024-06-03 19:49:46 -0300

    first commit

---
+++ src/main/java/com/tabmedd/TabmedbackApplication.java
@@ -0,0 +1,13 @@
+package com.tabmedd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabmedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabmedbackApplication.class, args);
+	}
+
+}
