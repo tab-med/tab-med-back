44aee3455c5a7ea1a68120cfadd89fc3cca51f94
arthur v <hayashiifunky@gmail.com>
2024-06-10 20:40:29 -0300

    feat: criado a pasta model e os primeiros metodos crud tambem foram criados

---
+++ src/main/java/com/tabmedd/Controller/ApiControllers.java
@@ -0,0 +1,44 @@
+package com.tabmedd.Controller;
+
+import com.tabmedd.Model.User;
+import com.tabmedd.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ApiControllers {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/")
+    public String getPage() {
+        return "Welcome";
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+    @PostMapping("/save")
+    public String saveUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "Saved...";
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user) {
+        User updateUser = userRepo.findById(id).get();
+        updateUser.setFirstName(user.getFirstName());
+        updateUser.setLastName(user.getLastName());
+        updateUser.setCpf(user.getCpf());
+        updateUser.setAge(user.getAge());
+        updateUser.setOccupation(user.getOccupation());
+        userRepo.save(updateUser);
+        return "Updated...";
+    }
+
+}
---
+++ src/main/java/com/tabmedd/Model/User.java
@@ -0,0 +1,34 @@
+package com.tabmedd.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String cpf;
+
+    @Column
+    private int age;
+
+    @Column
+    private String occupation;
+}
